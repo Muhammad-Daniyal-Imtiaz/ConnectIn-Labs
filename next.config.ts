d7612eafa29ba6b92a9a6071dcf2175909babee1
Muhammad Daniyal Imtiaz <muhammaddaniimtiaz@gmail.com>
2026-06-05 14:32:23 +0500

    Cloudfare deployment in Progress

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,19 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: "standalone",
+  images: {
+    unoptimized: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
   experimental: {
     serverActions: {
-      bodySizeLimit: '10mb',
+      bodySizeLimit: "10mb",
     },
   },
 };
